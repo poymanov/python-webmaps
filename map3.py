14c63b6d2a5501147c5800fb110dd2e31aa76199
Nicolay Poymanov <n.poymanov@gmail.com>
2018-07-15 16:24:55 +0300

    Add population colored layer

--- map3.py
+++ map3.py
@@ -2,6 +2,8 @@ import folium
 import pandas
 
 webmap = folium.Map(location=[53.601353, -34.222003], zoom_start=2, tiles='Mapbox Bright')
-webmap.add_child(folium.GeoJson(data=(open('files/world.json', 'r', encoding='utf-8-sig')).read()))
+webmap.add_child(folium.GeoJson(data=(open('files/world.json', 'r', encoding='utf-8-sig')).read(),
+	style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000 
+	else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 webmap.save('map3.html')
