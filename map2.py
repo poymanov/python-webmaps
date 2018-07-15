7a838e42023c8640a14fdb95f8882ccc2cf1bd42
Nicolay Poymanov <n.poymanov@gmail.com>
2018-07-15 14:32:45 +0300

    Add elevation data

--- map2.py
+++ map2.py
@@ -7,11 +7,12 @@ data = pandas.read_csv('files/volcanoes.txt')
 
 lat = list(data['LAT'])
 lon = list(data['LON'])
+elev = list(data['ELEV'])
 
 fg = folium.FeatureGroup(name='My Feature Group')
 
-for lt, ln in zip(lat, lon):
-	fg.add_child(folium.Marker(location=[lt, ln]))
+for lt, ln, el in zip(lat, lon, elev):
+	fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + ' m'))
 
 webmap.add_child(fg)
 webmap.save('map2.html')
