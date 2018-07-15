dfaa5f5229b501914114c40516dbf16e24c0ca0b
Nicolay Poymanov <n.poymanov@gmail.com>
2018-07-15 13:52:48 +0300

    Add multiple markers

--- map1.py
+++ map1.py
@@ -3,7 +3,11 @@ import folium
 webmap = folium.Map(location=[38.931303, -77.049646], zoom_start=8, tiles='Mapbox Bright')
 
 fg = folium.FeatureGroup(name='My Feature Group')
-fg.add_child(folium.Marker(location=[38.931303, -77.049646], popup='My Marker', icon=folium.Icon(color='green')))
+
+coordinates = [[38.931303, -77.049646], [38.992155, -80.247268]]
+
+for coordinate in coordinates:
+	fg.add_child(folium.Marker(location=coordinate, popup='My Marker', icon=folium.Icon(color='green')))
 
 webmap.add_child(fg);
 webmap.save('map1.html')
