79eeb7dc3d237142351d32b6c20323c66aceaffe
Nicolay Poymanov <n.poymanov@gmail.com>
2018-07-15 14:44:15 +0300

    Add color popups

--- map2.py
+++ map2.py
@@ -1,6 +1,14 @@
 import folium
 import pandas
 
+def elevation_color_status(elevation):
+	if elevation < 1000:
+		return 'green'
+	elif 1000 <= elevation < 3000:
+		return 'orange'
+	else:
+		return 'red'
+
 webmap = folium.Map(location=[39.656523, -113.065719], zoom_start=5, tiles='Mapbox Bright')
 
 data = pandas.read_csv('files/volcanoes.txt')
@@ -12,7 +20,7 @@ elev = list(data['ELEV'])
 fg = folium.FeatureGroup(name='My Feature Group')
 
 for lt, ln, el in zip(lat, lon, elev):
-	fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + ' m'))
+	fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + ' m', icon=folium.Icon(color=elevation_color_status(el))))
 
 webmap.add_child(fg)
 webmap.save('map2.html')
